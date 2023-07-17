75e683f3af7bf20fa247bf7d8393087145d139ae
Hiep Vo <hiep_vo@datahouse.com>
2023-07-17 23:23:38 +0700

    setting prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   signup() {
     return { mess: 'I am signup' };
